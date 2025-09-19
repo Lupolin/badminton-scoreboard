ad884a5b800bbccd828ada2c6da41e1429c84537
Lucas <lucas74520@gmail.com>
2025-09-19 16:52:01 +0800

    調整nginx，優先上線

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ from .config import load_config
 from .routes.web import web_bp
 from .routes.api import api_bp
 
-
 def create_app():
     app = Flask(__name__)
     cfg = load_config()
@@ -20,9 +19,24 @@ def create_app():
     app.register_blueprint(web_bp)
     app.register_blueprint(api_bp, url_prefix="/api")
 
-    # ⭐ 把應用包在 /badminton-scoreboard 下
-    app.wsgi_app = DispatcherMiddleware(Flask("dummy"), {
-        "/badminton-scoreboard": app.wsgi_app
-    })
-
     return app
+
+
+# def create_app():
+#     app = Flask(__name__)
+#     cfg = load_config()
+#     app.config.update(cfg)
+#     app.secret_key = cfg.get("FLASK_SECRET", "dev-secret")
+
+#     CORS(app)
+
+#     # Blueprints
+#     app.register_blueprint(web_bp)
+#     app.register_blueprint(api_bp, url_prefix="/api")
+
+#     # ⭐ 把應用包在 /badminton-scoreboard 下
+#     app.wsgi_app = DispatcherMiddleware(Flask("dummy"), {
+#         "/badminton-scoreboard": app.wsgi_app
+#     })
+
+#     return app
