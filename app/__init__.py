f18d40499846202d0d02cfc4d14ae6b798a3eea5
Lucas <lucas74520@gmail.com>
2025-09-19 16:24:59 +0800

    修改prefix

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,12 @@
 import os
 from flask import Flask
 from flask_cors import CORS
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
 from .config import load_config
 from .routes.web import web_bp
 from .routes.api import api_bp
+
 
 def create_app():
     app = Flask(__name__)
@@ -16,5 +19,10 @@ def create_app():
     # Blueprints
     app.register_blueprint(web_bp)
     app.register_blueprint(api_bp, url_prefix="/api")
+
+    # ⭐ 把應用包在 /badminton-scoreboard 下
+    app.wsgi_app = DispatcherMiddleware(Flask("dummy"), {
+        "/badminton-scoreboard": app.wsgi_app
+    })
 
     return app
